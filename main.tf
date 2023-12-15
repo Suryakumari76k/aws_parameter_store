88c99bbd6d977439a484b41b0523e97ebcf04107
sriram <munukutla2001@gmail.com>
2023-12-15 11:23:03 +0530

    project

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "aws_ssm_parameter" "f11" {
+  count = length(var.params)
+  name = var.params[count.index].name
+  type = var.params[count.index].type
+  value = var.params[count.index].value
+}
+
+variable "params" {
+  default = [
+    { name: "Seetha", type: "String", value: "Seetha@123" },
+    { name: "Gopal", type: "String", value: "Gopal@123" },
+  ]
+}
